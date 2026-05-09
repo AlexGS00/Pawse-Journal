c22361cc3188b9e1fdc0642be2f0456046d1c63b
AlexGS00 <acortojan@gmail.com>
2026-05-09 14:51:51 +0300

    add edit urls

--- pawse/journal/urls.py
+++ pawse/journal/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("journal/new/", views.create_entry, name="create_entry"),
+    path("journal/<int:entry_id>/", views.entry_detail, name="entry_detail"),
+    path("journal/<int:entry_id>/edit/", views.edit_entry, name="edit_entry"),
     path("register", views.register, name="register"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
