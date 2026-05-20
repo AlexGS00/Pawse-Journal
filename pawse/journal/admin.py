4858aebe9dd5b79166967597f50ebd6da60cccb2
AlexGS00 <acortojan@gmail.com>
2026-05-20 19:45:40 +0300

    create admin pannel

--- pawse/journal/admin.py
+++ pawse/journal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Entry, EntryChunck, Tag, Conversation, Message
 
-# Register your models here.
+admin.site.register(Entry)
+admin.site.register(EntryChunck)
+admin.site.register(Tag)
+admin.site.register(Conversation)
+admin.site.register(Message)
